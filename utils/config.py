80c618339084d7e7051f64fc12fcf56076270583
Prathishriyan <prathikshashriyan4@gmail.com>
2026-04-16 12:12:35 +0530

    changes a code in config.py

--- utils/config.py
+++ utils/config.py
@@ -1,7 +1,8 @@
 import os
 from dotenv import load_dotenv
+import streamlit as st
 
 load_dotenv()
 
-USAJOBS_API_KEY = os.getenv("USAJOBS_API_KEY")
-GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
+USAJOBS_API_KEY = st.secrets["USAJOBS_API_KEY"]
+GEMINI_API_KEY = st.secrets["GEMINI_API_KEY"]
